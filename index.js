734fb95c9831770db57c6dd11fcc43d06d52d48a
Alex Sloth <leda_sasha@mail.ru>
2021-03-25 18:27:59 +0200

    install lodash / add .gitignore

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hello, Hexlet!");
+// По общепринятому соглашению, lodash импортируется под именем _
+import _ from "lodash";
+
+// Пересечение массивов
+//console.log(_.intersection([2, 1], [2, 3]));
+
+// capitalize делает первую букву заглавной
+//console.log(_.capitalize("hello"));
+
+console.log(_.last(['one', 'two']));
